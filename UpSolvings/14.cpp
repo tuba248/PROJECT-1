0077415ad597323467e1c0a06bab57487c3af0ce
tuba248 <94109596+tuba248@users.noreply.github.com>
2022-01-14 18:31:31 +0600

    Add files via upload

---
+++ UpSolvings/14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int h;
+    cin>>h;
+    if(h<=12){
+        cout<<"Lark";
+    } else {
+        cout<<"Owl";
+    }
+    return 0;
+
+}
