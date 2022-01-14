a3d744769eb2b04278c11e60c13e6f14050d4f84
tuba248 <94109596+tuba248@users.noreply.github.com>
2022-01-14 17:43:33 +0600

    Create a2.cpp

---
+++ lab2/a2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+int x;
+cin>>x;
+if (x%2==0 && x!=0){
+    cout<<"Even";
+
+}else{
+    if(x!=0){
+        cout<<"Odd";
+    }
+    
+   
+}if(x==0){
+         cout<<"None";
+}
+
+return 0;
+}
